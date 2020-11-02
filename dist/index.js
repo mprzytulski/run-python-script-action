c8ea3a5b61eafa10db331a3a477459e6efdcc3dd
Michal Przytulski <michal@przytulski.pl>
2020-11-02 14:41:30 +0100

    terminate with subprocess exit code

--- dist/index.js
+++ dist/index.js
@@ -12,15 +12,22 @@ const tmp = __webpack_require__(517);
 
 async function run() {
     tmp.setGracefulCleanup();
-    const script = core.getInput('script');
+    // const script = core.getInput('script');
+    const script = "import sys; sys.exit(1)"
     const filename = tmp.tmpNameSync({postfix: '.py'});
     fs.writeFileSync(filename, script);
-    exec.exec('python', [filename])
+    await exec.exec('python', [filename])
 }
 
+
 run().catch((e) => {
-    console.error(e.message)
-});
+    if (/failed with exit code (\d+)/.test(e)) {
+        let exit_code = /failed with exit code (\d+)/.exec(e)[1];
+        process.exit(exit_code)
+    }
+}).then(() => {
+    console.log("Done")
+})
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const tmp = require('tmp');
 
 async function run() {
     tmp.setGracefulCleanup();
-    const script = core.getInput('script');
+    // const script = core.getInput('script');
+    const script = "import sys; sys.exit(1)"
     const filename = tmp.tmpNameSync({postfix: '.py'});
     fs.writeFileSync(filename, script);
-    exec.exec('python', [filename])
+    await exec.exec('python', [filename])
 }
 
+
 run().catch((e) => {
-    console.error(e.message)
-});
+    if (/failed with exit code (\d+)/.test(e)) {
+        let exit_code = /failed with exit code (\d+)/.exec(e)[1];
+        process.exit(exit_code)
+    }
+}).then(() => {
+    console.log("Done")
+})
